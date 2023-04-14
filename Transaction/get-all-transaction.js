490710ef5f6ed60c336f17bb4464aa180606c59d
Ankit Singh <ankit.singh.zm@gmail.com>
2023-04-14 14:00:39 +0530

    converted files to modules

---
+++ Transaction/get-all-transaction.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import CryptoJS from "crypto-js";
+import moment from "moment";
+import dotenv from "dotenv";
+dotenv.config()
+
+const URL = 'https://sandbox.thirdfi.org/api/v1/transaction'
+const METHOD = 'GET'
+const timestamp = moment().unix()
+let baseString = `${URL}&method=${METHOD}&timestamp=${timestamp}`
+let data = '';
+
+const hash = CryptoJS.HmacSHA256(baseString, process.env.SECRET).toString(
+  CryptoJS.enc.Hex
+)
+
+let config = {
+  method: 'get',
+  maxBodyLength: Infinity,
+  url: URL,
+  headers: {
+    'x-sec-key': process.env.API_KEY,
+    'x-sec-ts': timestamp,
+    'x-sec-sign': hash
+  },
+  data: data
+};
+
+axios.request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
